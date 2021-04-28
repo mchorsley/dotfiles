62931fd427784ef9c22cd9ad05df523e8bd8397e
Martin Horsley <mchorsley@gmail.com>
2021-04-28 07:18:28 +0100

    publicip alias displays public ip address

--- aliases.zsh
+++ aliases.zsh
@@ -6,6 +6,7 @@ alias ll="/usr/local/opt/coreutils/libexec/gnubin/ls -AhlFo --color --group-dire
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias c="clear"
+alias reload="source ~/.profile"
 
 # Directories
 alias dotfiles="cd $DOTFILES"
@@ -30,13 +31,8 @@ alias composer="php -d memory_limit=-1 /usr/local/bin/composer"
 alias nfresh="rm -rf node_modules/ package-lock.json && npm install"
 alias watch="npm run watch"
 
-# Vagrant
-# alias v="vagrant global-status"
-# alias vup="vagrant up"
-# alias vhalt="vagrant halt"
-# alias vssh="vagrant ssh"
-# alias vreload="vagrant reload"
-# alias vrebuild="vagrant destroy --force && vagrant up"
+# Network
+alias publicip="dig +short myip.opendns.com @resolver1.opendns.com"
 
 # Docker
 alias docker-composer="docker-compose"
